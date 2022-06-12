43ee6e86bb97a7c656c5bf00db58a6520ef08edd
Ronald Abu Saleh <lancerrnld@gmail.com>
2022-06-12 19:13:20 -0400

    Get drivers by queries route improvements

--- src/routes/drivers/drivers.js
+++ src/routes/drivers/drivers.js
@@ -33,19 +33,49 @@ export const getDriverIdByIdentificationCode = async (code) => {
 export const getDriversByQueries = async (queries) => {
   try {
     const {
-      search: { fullname, identificationCode },
+      search: { page, fullname, identificationCode },
       fields,
     } = queries;
 
     const joined = fields.join(", ");
 
-    if (fullname) {
-      return await query(`SELECT ${joined} FROM driver WHERE Name LIKE ?`, [fullname + "%"]);
+    let sql1 = `SELECT ${joined} FROM driver`;
+    let sql2 = `SELECT COUNT(IDDriver) as counter FROM driver`;
+    const params1 = [];
+
+    if (fullname && !identificationCode) {
+      sql1 += " WHERE Name LIKE ?";
+      params1.push(fullname + "%");
+
+      sql2 += " WHERE Name LIKE ?";
     }
 
-    if (identificationCode) {
-      return await query(`SELECT ${joined} FROM driver WHERE IdentificationCode LIKE ?`, [identificationCode + "%"]); /* prettier-ignore */
+    if (identificationCode && !fullname) {
+      sql1 += " WHERE IdentificationCode LIKE ?";
+      params1.push(identificationCode + "%");
+
+      sql2 += " WHERE IdentificationCode LIKE ?";
     }
+
+    if (page) {
+      const counter = await query(sql2, params1);
+
+      const limit = 8;
+      const offset = Number(page) * limit - limit;
+
+      sql1 += " LIMIT ? OFFSET ?";
+      params1.push(limit);
+      params1.push(offset);
+
+      return {
+        drivers: await query(sql1, params1),
+        counter: counter[0].counter,
+      };
+    }
+
+    return {
+      drivers: await query(sql1, params1),
+    };
   } catch (error) {
     return false;
   }
