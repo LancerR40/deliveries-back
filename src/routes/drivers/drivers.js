64279e0c6042bc31090a910b642b9b7ad1be736e
Ronald Abu Saleh <lancerrnld@gmail.com>
2022-06-19 01:32:09 -0400

    Small change

--- src/routes/drivers/drivers.js
+++ src/routes/drivers/drivers.js
@@ -34,7 +34,7 @@ export const getDriversByQueries = async (queries) => {
   try {
     const { search } = queries;
 
-    let sql1 = `SELECT IDDriver as driverId, Name as name, Lastname as lastname, IdentificationCode as identificationCode, Photo as photo, Email as email, CreatedAt as createdAt FROM driver`; /* prettier-ignore */
+    let sql1 = `SELECT IDDriver as driverId, Name as name, Lastname as lastname, IdentificationCode as identificationCode, Gender as gender, Photo as photo, Email as email, CreatedAt as createdAt FROM driver`; /* prettier-ignore */
     let sql2 = `SELECT COUNT(IDDriver) as counter FROM driver`;
     const params = [];
 
